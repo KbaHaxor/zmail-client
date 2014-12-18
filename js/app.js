edd938554bb99580d6abfa33cdbf888b8f3ee5cf
Kiffin Gish <kiffin.gish@planet.nl>
2014-12-18 19:34:45 +0100

    No more belongsTo for App.Key data model because of problems with RESTAdapter, commented out.

--- js/app.js
+++ js/app.js
@@ -9,10 +9,67 @@ App = Ember.Application.create({
 //Ember.LOG_BINDINGS = true;
 
 /** ADAPTERS **/
+//App.ApplicationAdapter = DS.FixtureAdapter.extend({});
+
 App.ApplicationAdapter = DS.RESTAdapter.extend({});
 
 DS.RESTAdapter.reopen({
-    host: 'http://0.0.0.0:5000'
+//    host: 'http://0.0.0.0:5000'
+    host: 'http://192.168.2.9:5000'
+});
+
+App.FoldersAdapter = DS.RESTAdapter.extend({
+    buildURL: function(type, id, record) {
+        var url = this._super(type, id, url);
+//        console.log('FoldersAdapter: buildURL(type='+type+',id='+id+') => '+url);
+        return url;
+    }
+});
+
+App.FolderAdapter = DS.RESTAdapter.extend({
+    buildURL: function(type, id, record) {
+        var url = this._super(type, id, url);
+//        console.log('FolderAdapter: buildURL(type='+type+',id='+id+') => '+url);
+        return url;
+    }
+});
+
+App.ItemsAdapter = DS.RESTAdapter.extend({
+    buildURL: function(type, id, record) {
+        var url = this._super(type, id, url);
+//        console.log('ItemsAdapter: buildURL(type='+type+',id='+id+') => '+url);
+        return url;
+    }
+});
+
+App.ItemAdapter = DS.RESTAdapter.extend({
+    buildURL: function(type, id, record) {
+        var url = this._super(type, id, url);
+//        console.log('ItemAdapter: buildURL(type='+type+',id='+id+') => '+url);
+        return url;
+    }
+});
+
+App.KeysAdapter = DS.RESTAdapter.extend({
+    buildURL: function(type, id, record) {
+        var url = this._super(type, id, url);
+//        console.log('KeysAdapter: buildURL(type='+type+',id='+id+') => '+url);
+        return url;
+    }
+});
+
+/** SERIALIZERS **/
+App.FolderSerializer = DS.RESTSerializer.extend({
+    typeForRoot: function(root) {
+        var res = this._super(root);
+        console.log('FolderSerializer: typeForRoot(root='+root+') => '+res);
+        return res;
+    },
+    normalize: function(type, hash, prop) {
+        var res = this._super(type, hash, prop);
+//        console.log('FolderSerializer: normalize(type='+type+',hash='+JSON.stringify(hash)+',prop='+JSON.stringify(prop)+') => '+JSON.stringify(res));
+        return res;
+    }
 });
 
 /** ROUTER MAP **/
@@ -38,7 +95,7 @@ App.IndexRoute = Ember.Route.extend({
 
 App.FoldersRoute = Ember.Route.extend({
     model: function() {
-        console.log('FoldersRoute: model');
+        console.log('FoldersRoute: model()');
         return this.store.find('folder');
     }
 });
@@ -72,7 +129,8 @@ App.FolderIndexRoute = Ember.Route.extend({
 
 App.ItemsRoute = Ember.Route.extend({
     model: function() {
-        console.log('ItemsRoute: model');
+        console.log('ItemsRoute: model()');
+//        console.table(App.__container__.lookup('store:main').typeMapFor(App.Item).records);
         return this.modelFor('folder').get('items');
     }
 });
@@ -99,15 +157,17 @@ App.ItemIndexRoute = Ember.Route.extend({
 
 App.KeysRoute = Ember.Route.extend({
     model: function() {
-        console.log('KeysRoute: model');
+        console.log('KeysRoute: model()');
+//        console.table(App.__container__.lookup('store:main').typeMapFor(App.Item));
         return this.modelFor('item').get('keys');
     }
+
 });
 
 /** CONTROLLERS **/
 App.FoldersController = Ember.ArrayController.extend({
-    sortAscending: true,
-    sortProperties: ['name'],
+    //sortAscending: true,
+    //sortProperties: ['name'],
     itemController: 'folder'
 });
 
@@ -145,8 +205,8 @@ App.FolderController = Ember.ObjectController.extend({
 });
 
 App.ItemsController = Ember.ArrayController.extend({
-    sortAscending: true,
-    sortProperties: ['name'],
+    //sortAscending: true,
+    //sortProperties: ['name'],
     itemController: 'item'
 });
 
@@ -156,7 +216,7 @@ App.ItemController = Ember.ObjectController.extend({
 App.KeysController = Ember.ArrayController.extend({
 //    filter: 'PR_',
     sortAscending: true,
-    sortProperties: ['name']
+    sortProperties: ['name'],
 //    filteredContent: function() {
 //        var filter = this.get('filter');
 //        var keys = this.get('arrangedContent');
@@ -172,7 +232,7 @@ App.KeysController = Ember.ArrayController.extend({
 //                    name = name.substr(len);
 //                    k.set('name', name);
 //                }
-//            console.log('KeysController: filterContent(name='+name+',value='+value+') => '+ok);
+////            console.log('KeysController: filterContent(name='+name+',value='+value+') => '+ok);
 //            return ok;
 //        });
 //    }.property('arrangedContent', 'filter')
@@ -195,8 +255,8 @@ App.Item = DS.Model.extend({
 
 App.Key = DS.Model.extend({
     name: DS.attr('string'),
-    value: DS.attr('string'),
-    item: DS.belongsTo('item', {async: true} )
+    value: DS.attr('string')
+//    item: DS.belongsTo('item', {async: true} )
 });
 
 /** HANDLEBAR HELPERS **/
@@ -224,3 +284,71 @@ Ember.Handlebars.helper('prefixstrip', function(value, options) {
     var re = new RegExp('^'+prefix);
     return value.replace(re, '');
 });
+
+/** FIXTURES **/
+App.Folder.reopenClass({
+  FIXTURES: [
+    { id: 1, name: 'Sent items', ftype: 'none',            count: 3, items: [51, 52] },
+    { id: 2, name: 'Notes',      ftype: 'pf.note',         count: 4, items: [53, 54, 55, 56] },
+    { id: 3, name: 'Inbox',      ftype: 'none',            count: 1, items: [57] },
+    { id: 4, name: 'Calendar',   ftype: 'ipf.appointment', count: 0, items: [] },
+    { id: 5, name: 'Journal',    ftype: 'ipf.journal',     count: 2, items: [58, 59] }
+  ]
+});
+
+App.Item.reopenClass({
+  FIXTURES: [
+    { id: 51, name: 'Fifty-one',   folder: 1, count: 2, keys: [101, 102] },
+    { id: 52, name: 'Fifty-two',   folder: 1, count: 0, keys: [] },
+    { id: 53, name: 'Fifty-three', folder: 2, count: 1, keys: [103] },
+    { id: 54, name: 'Fifty-four',  folder: 2, count: 1, keys: [104] },
+    { id: 55, name: 'Fifty-five',  folder: 2, count: 1, keys: [105] },
+    { id: 56, name: 'Fifty-six',   folder: 2, count: 3, keys: [106, 107, 108] },
+    { id: 57, name: 'Fifty-seven', folder: 3, count: 0, keys: [] },
+    { id: 58, name: 'Fifty-eight', folder: 5, count: 2, keys: [109, 110] },
+    { id: 59, name: 'Fifty-nine',  folder: 5, count: 1, keys: [111] }
+  ]
+});
+
+App.Key.reopenClass({
+  FIXTURES: [
+    { id: 101, name: 'Hundred-one',    value: 'Future famous novel',          item: 51 },
+    { id: 102, name: 'Hundred-two',    value: 'Something to think about',     item: 51 },
+    { id: 103, name: 'Hundred-three',  value: 'A storm in a teacup',          item: 53 },
+    { id: 104, name: 'Hundred-four',   value: 'Keep up the good work',        item: 54 },
+    { id: 105, name: 'Hundred-five',   value: 'One small step for man',       item: 55 },
+    { id: 106, name: 'Hundred-six',    value: 'Who can tell me more?',        item: 56 },
+    { id: 107, name: 'Hundred-seven',  value: 'That is the name of the game', item: 56 },
+    { id: 108, name: 'Hundred-eight',  value: 'Will be back tomorrow',        item: 56 },
+    { id: 109, name: 'Hundred-nine',   value: 'Sweep it under the rug',       item: 58 },
+    { id: 110, name: 'Hundred-ten',    value: 'You must be kidding me',       item: 58 },
+    { id: 111, name: 'Hundred-eleven', value: 'An apple a day is healthy',    item: 59 }
+  ]
+});
+
+/** HANDLEBAR HELPERS **/
+Ember.Handlebars.helper('formatvalue', function(value, options) {
+    if (value) {
+        var maxlen = options.hash.maxlen || 50;
+        if (/\s/g.test(value)) {
+            return value;
+        } else if (value.length > maxlen) {
+            return value.substring(0,maxlen-4) + '...';
+        } else {
+            return value;
+        }
+    } else {
+        return 'null';
+    }
+});
+
+Ember.Handlebars.helper('prefixstrip', function(value, options) {
+    var prefix = options.hash.prefix;
+    if (!prefix) {
+//        console.log('prefixstrip => prefix missing!');
+        return value;
+    }
+    var re = new RegExp('^'+prefix);
+    return value.replace(re, '');
+});
+
